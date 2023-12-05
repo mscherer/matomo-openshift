7d7bfc079b383fed669416a126f94e5c6d39bb70
Michael Scherer <misc@redhat.com>
2023-12-05 15:33:43 +0100

    Do not use autoinstallation until it is working

--- php/init.sh
+++ php/init.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+set -x
 cd /srv/matomo/
-# TODO do the init if not present
-php /usr/local/bin/install.php
+if [ -v MATOMO_AUTOINST ]; then
+	if [ ! -f /srv/matomo/config/config.ini.php ]; then
+		cp /srv/matomo/config/global.ini.php /srv/matomo/config/config.ini.php
+		sed -i -e "s/\[PluginsInstalled\]/\nPlugins\[\] = UnattendedInstall\n\[PluginsInstalled\]\nPluginsInstalled\[\] = UnattendedInstallation\n/" /srv/matomo/config/config.ini.php
 
-php-fpm -F
+		#cat >> /srv/matomo/config/config.ini.php <<EOF
+		#[Plugins]
+		#Plugins[] = UnattendedInstall
+		#[PluginsInstalled]
+		#PluginsInstalled[] = UnattendedInstallation
+		#EOF
+
+		php ./console plugin:list
+		#php ./console plugin:activate UnattendedInstallation
+		php ./console plugin:list
+		php ./console
+	fi;
+fi;
+php-fpm -F -R
